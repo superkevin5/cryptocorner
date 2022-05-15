e33b42764bcfc6d0e0301749212e2f0d1212b3ca
superkevin5 <hlmloveaustralia@gmail.com>
2022-05-15 16:26:04 +0800

    init

---
+++ src/components/Hello.tsx
@@ -0,0 +1,22 @@
+import React, { Component, PureComponent } from "react";
+
+type MyProps = {
+    // using `interface` is also ok
+    message: string;
+};
+type MyState = {
+    count: number; // like this
+};
+export class Hello extends React.Component<MyProps, MyState> {
+    state: MyState = {
+        // optional second annotation for better type inference
+        count: 0,
+    };
+    render() {
+        return (
+            <div>
+                {this.props.message} {this.state.count}
+            </div>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    devServer: {
+        historyApiFallback: true,
+        compress: true,
+        static: './',
+        hot: true,
+        port: 9001
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "public/index.html",
+        hash: true, // cache busting
+        filename: '../dist/index.html'
+    })],
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: { loader: "babel-loader" }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            // All files with a z'.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because ist allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+};
